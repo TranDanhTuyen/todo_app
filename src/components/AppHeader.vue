252951734e5bb6fd8b3e62fd3edce25e4ff2f3e0
tuyentd <tuyentd.dbtnn@gmail.com>
2022-04-11 14:02:30 +0700

    ok

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <appheader class="app-header">
+      <h1>Việc cần làm</h1>
+  </appheader>
+</template>
+
+<script>
+export default {
+    name: "AppHeader"
+}
+</script>
+
+<style>
+.app-header {
+    background: #333;
+    color: rgb(18, 1, 1);
+    text-align: center;
+    padding: 10px;
+}
+</style>
