28978d4a3e78f6148a3ff0e26f4717b78c4fa163
tuyentd <tuyentd.dbtnn@gmail.com>
2022-04-11 16:26:28 +0700

    Xong phan 2

--- src/components/AppTodos.vue
+++ src/components/AppTodos.vue
@@ -1,12 +1,38 @@
 <template>
-  <p>Viec 1</p>
-  <p>Viec 2</p>
-  <p>Viec 3</p>
+  <TodoItem v-for="todo in todos" v-bind:key = "todo.id" v-bind:todoProps = "todo"/>
 </template>
 
-<script>
+<script>    
+import { ref } from "vue";
+import TodoItem from "./TodoItem";
+
 export default {
     name: "AppTodos",
+    components: {
+        TodoItem,
+    },
+    setup () {
+        const todos = ref([
+            {
+                id: 1,
+                title: 'Việc 01',
+                completed: true,
+            },
+            {
+                id: 2,
+                title: 'Việc 02',
+                completed: false,
+            },
+            {
+                id: 3,
+                title: 'Việc 03',
+                completed: true,
+            }
+            ]);
+        return {
+            todos,
+        }
+    }
 }
 </script>
 
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,33 @@
+<template>
+    <p :class = "['todo-item', todoProps.completed ? 'is-completed' : '']">
+      <input type="checkbox" :checked = "todoProps.completed"/>
+      {{todoProps.title}}
+      <button class="del-btn">Delete</button>
+    </p>
+</template>
+
+<script>
+export default {
+    name: "TodoItem",
+    props: ['todoProps'],
+}
+</script>
+
+<style>
+.todo-item {
+    background: #f4f4f4;
+    padding: 10px;
+    margin: 0;
+    border-bottom: 1px #ccc dotted;
+}
+.del-btn {
+    background: #ff0000;
+    color: #fff;
+    border: none;
+    float: right;
+    cursor: pointer;
+}
+.is-completed {
+    text-decoration: line-through;
+}
+</style>
