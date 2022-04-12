057489e1e4fd17f6a96a975fc6e0c2c86d2a22df
tuyentd <tuyentd.dbtnn@gmail.com>
2022-04-12 10:22:43 +0700

    Xong phần 4

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,54 @@
+<template>
+  <form @submit="addItem">
+      <input type="text" placeholder="Việc mới ..." v-model="title" @input="checkInputValue"/>
+      <input type="submit" value="Thêm" class="add-btn"/>
+  </form>
+</template>
+
+<script>
+import {ref} from "vue";
+import {v4 as uuidv4} from "uuid";
+
+export default {
+    name: "AddTodo",
+    setup(props, context) {
+        const title = ref('');
+
+        const addItem = event => {
+            event.preventDefault();
+            const newItem = {
+                id: uuidv4(),
+                title: title.value,
+                completed: false,
+            }
+            context.emit('add-todo', newItem);
+            title.value = '';
+        }
+
+        const checkInputValue = () => {
+            console.log(title.value);
+        }
+
+        return {
+            title,
+            checkInputValue,
+            addItem,
+        }
+    }
+}
+</script>
+
+<style>
+form {
+    display: flex;
+}
+
+input[type = "text"] {
+    flex: 10;
+    padding: 5px;
+}
+
+input[type = "submit"] {
+    flex: 2;
+}
+</style>
--- src/components/AppTodos.vue
+++ src/components/AppTodos.vue
@@ -1,36 +1,40 @@
 <template>
-  <TodoItem 
-  v-for="todo in todos" 
-  v-bind:key = "todo.id" 
-  v-bind:todoProps = "todo"
-  v-on:item-completed = "markCompleted"
-  v-on:item-deleted = "deleteTodo"
+    <AddTodo @add-todo = "addTodo"/>
+    <TodoItem 
+    v-for="todo in todos" 
+    v-bind:key = "todo.id" 
+    v-bind:todoProps = "todo"
+    v-on:item-completed = "markCompleted"
+    v-on:item-deleted = "deleteTodo"
   />
 </template>
 
 <script>    
 import { ref } from "vue";
 import TodoItem from "./TodoItem";
+import AddTodo from "./AddTodo";
+import {v4 as uuidv4} from "uuid";
 
 export default {
     name: "AppTodos",
     components: {
         TodoItem,
+        AddTodo,
     },
     setup () {
         const todos = ref([
             {
-                id: 1,
+                id: uuidv4(),
                 title: 'Việc 01',
                 completed: false,
             },
             {
-                id: 2,
+                id: uuidv4(),
                 title: 'Việc 02',
                 completed: false,
             },
             {
-                id: 3,
+                id: uuidv4(),
                 title: 'Việc 03',
                 completed: false,
             }
@@ -44,10 +48,15 @@ export default {
         const deleteTodo = (id) => {
             todos.value = todos.value.filter(todo => todo.id !== id);
         }
+        const addTodo = (newTodo) => {
+            console.log(newTodo.id);
+            todos.value.push(newTodo);
+        } 
         return {
             todos,
             markCompleted,
             deleteTodo,
+            addTodo,
         }
     }
 }
